4dcf57636e3f36117f82285a10f8ede74b45402c
Fabiano Brito <fabianopbrito@gmail.com>
2019-12-28 16:58:21 -0300

    Remove square brackets from server config.

--- packages/postgresql-server/server/db/config.js
+++ packages/postgresql-server/server/db/config.js
@@ -1,22 +1,22 @@
 module.exports = {
   development: {
-    username: '[database_username]',
+    username: 'database_username',
     password: null,
-    database: '[database_development]',
+    database: 'database_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
-    username: '[database_username]',
+    username: 'database_username',
     password: null,
-    database: '[database_test]',
+    database: 'database_test',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
-    username: '[database_username]',
+    username: 'database_username',
     password: null,
-    database: '[database_production]',
+    database: 'database_production',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
