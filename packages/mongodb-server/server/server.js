7eead809c1ec8a5d09c2e1eb99e5ac88ebdfad6c
Fabiano Brito <fabianopbrito@gmail.com>
2020-01-01 17:07:47 -0300

    Remove comments and deprecation warnings.

--- packages/mongodb-server/server/server.js
+++ packages/mongodb-server/server/server.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
-// const populateDatabase from "../scripts/populateDatabase";
 const app = require('./app');
 
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/test_database';
 const port = process.env.PORT || 9000;
 
 (async () => {
-  // Connect to the database
-  await mongoose.connect(url, { useNewUrlParser: true });
-  // Populate database with sample data if it's empty
-  // await populateDatabase();
-  // Start express App
+  await mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
   app.listen(port);
   console.log(`App listening on port ${port}...`);
 })();
