353b43aa8ae6053dce5bb419ca240d33088a9b0e
Fabiano Brito <fabianopbrito@gmail.com>
2020-01-04 23:33:59 -0300

    Run e2e for both servers.

--- test/e2e.js
+++ test/e2e.js
@@ -2,26 +2,33 @@
 const chalk = require('chalk');
 const cp = require('child_process');
 
+const servers = [
+  { path: 'mongodb', name: 'MongoDB' },
+  { path: 'postgresql', name: 'PostgreSQL' },
+];
+
 (() => {
-  console.log(
-    chalk.cyan('creating'),
-    chalk.yellow('MongoDB'),
-    chalk.cyan('sample from template...'),
-  );
-  cp.execSync('yarn create-sample postgresql', { stdio: 'inherit' });
+  servers.forEach(server => {
+    console.log(
+      chalk.cyan('creating'),
+      chalk.yellow(server.name),
+      chalk.cyan('sample from template...'),
+    );
+    cp.execSync(`yarn create-sample ${server.path}`, { stdio: 'inherit' });
 
-  console.log(chalk.cyan('Testing frontend...'));
-  cp.execSync('yarn test', { cwd: 'sample-app', stdio: 'inherit' });
+    console.log(chalk.cyan('Testing frontend...'));
+    cp.execSync('yarn test', { cwd: 'sample-app', stdio: 'inherit' });
 
-  console.log(chalk.cyan('Testing server...'));
-  cp.execSync('yarn test-server', { cwd: 'sample-app', stdio: 'inherit' });
+    console.log(chalk.cyan('Testing server...'));
+    cp.execSync('yarn test-server', { cwd: 'sample-app', stdio: 'inherit' });
 
-  console.log(chalk.cyan('Building frontend...'));
-  cp.execSync('yarn build', { cwd: 'sample-app', stdio: 'inherit' });
+    console.log(chalk.cyan('Building frontend...'));
+    cp.execSync('yarn build', { cwd: 'sample-app', stdio: 'inherit' });
 
-  console.log(chalk.cyan('Building server...'));
-  cp.execSync('yarn build-server', { cwd: 'sample-app', stdio: 'inherit' });
+    console.log(chalk.cyan('Building server...'));
+    cp.execSync('yarn build-server', { cwd: 'sample-app', stdio: 'inherit' });
 
-  console.log(chalk.cyan('cleaning sample...'));
-  cp.execSync('yarn clean', { stdio: 'inherit' });
+    console.log(chalk.cyan('cleaning sample...'));
+    cp.execSync('yarn clean', { stdio: 'inherit' });
+  });
 })();
