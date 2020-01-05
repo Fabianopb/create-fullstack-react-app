9d3391a9e2764f47a7b623decfb3ec76480391e8
Fabiano Brito <fabianopbrito@gmail.com>
2020-01-04 23:25:00 -0300

    Change config to use default postgres user.

--- templates/postgresql-server/server/db/config.js
+++ templates/postgresql-server/server/db/config.js
@@ -1,20 +1,20 @@
 module.exports = {
   development: {
-    username: 'database_username',
+    username: 'postgres',
     password: null,
     database: 'database_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
-    username: 'database_username',
+    username: 'postgres',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
-    username: 'database_username',
+    username: 'postgres',
     password: null,
     database: 'database_production',
     host: '127.0.0.1',
