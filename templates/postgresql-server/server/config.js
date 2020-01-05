3e46871c18a12b5eaf5589c5b2a8dd758cedbfef
Fabiano Brito <fabianopbrito@gmail.com>
2020-01-05 00:25:15 -0300

    Fix db role.

--- templates/postgresql-server/server/config.js
+++ templates/postgresql-server/server/config.js
@@ -1,20 +1,20 @@
 module.exports = {
   development: {
-    username: 'fabianobrito',
+    username: 'postgres',
     password: null,
     database: 'database_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
-    username: 'fabianobrito',
+    username: 'postgres',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   production: {
-    username: 'fabianobrito',
+    username: 'postgres',
     password: null,
     database: 'database_production',
     host: '127.0.0.1',
