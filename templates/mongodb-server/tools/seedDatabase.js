7c9e44fbe9cab8b4caf0acfc2dfd87d040669420
TaylorBeeston <beeston.taylor@gmail.com>
2020-05-13 13:53:16 -0700

    Fix seed script

--- templates/mongodb-server/tools/seedDatabase.js
+++ templates/mongodb-server/tools/seedDatabase.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const chalk = require('chalk');
-const Item = require('../server/items/item.model');
-const User = require('../server/users/user.model');
-const config = require('../server/config');
+import { connect, disconnect } from "mongoose";
+import chalk from "chalk";
+import Item from "../server/items/item.model";
+import User from "../server/users/user.model";
+import { url } from "../server/config";
 
 (async () => {
   try {
-    await mongoose.connect(config.url, {
+    await connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -14,25 +14,33 @@ const config = require('../server/config');
     const users = await User.find({});
     const items = await Item.find({});
     if (users.length === 0 && items.length === 0) {
-      console.log(chalk.yellow('No users or items in the database, creating sample data...'));
-      const user = new User({ name: 'John Doe', age: 34 });
+      console.log(
+        chalk.yellow(
+          "No users or items in the database, creating sample data..."
+        )
+      );
+      const user = new User({ name: "John Doe", age: 34 });
       await user.save();
-      console.log(chalk.green('Sample user successfuly created!'));
+      console.log(chalk.green("Sample user successfuly created!"));
       const newItems = [
-        { name: 'Paper clip', value: 0.1 },
-        { name: 'Colorful pen', value: 1.2 },
-        { name: 'Notebook', value: 2.5 },
-        { name: 'Soft eraser', value: 0.5 },
-        { name: 'Table lamp', value: 5.1 },
+        { name: "Paper clip", value: 0.1 },
+        { name: "Colorful pen", value: 1.2 },
+        { name: "Notebook", value: 2.5 },
+        { name: "Soft eraser", value: 0.5 },
+        { name: "Table lamp", value: 5.1 },
       ];
       await Item.insertMany(newItems);
-      console.log(chalk.green(`${newItems.length} item(s) successfuly created!`));
+      console.log(
+        chalk.green(`${newItems.length} item(s) successfuly created!`)
+      );
     } else {
-      console.log(chalk.yellow('Database already initiated, skipping populating script'));
+      console.log(
+        chalk.yellow("Database already initiated, skipping populating script")
+      );
     }
   } catch (error) {
     console.log(chalk.red(error));
   } finally {
-    await mongoose.disconnect();
+    await disconnect();
   }
 })();
